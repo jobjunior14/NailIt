611fcca1b9139d463653d697e9323eb6eb96d51a
jobjunior14 <juniorbisim@gmail.com>
2024-07-24 03:46:15 +0200

    adding dependencies

---
+++ client/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className=" bg-slate-700 flex-1">
+      <Text className=" font-bold text-white">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
